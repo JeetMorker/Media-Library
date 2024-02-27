28ea1ddbd3b34fc58159b06700a1051dad99ec99
rl856 <rl856@tux5.cci.drexel.edu>
2024-02-26 22:05:17 -0500

    included a grid into the html for books

--- bookscript.js
+++ bookscript.js
@@ -1,92 +1,92 @@
 const BooksArray = [
-  {
+{
 "Year Published": 1925.0,
     "Title": "The Great Gatsby",
     "Author": "F. Scott Fitzgerald",
     "Genre": "Tragedy,Literary Fiction",
     "Rating": "4/5",
     "Description": "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan." 
-      },
-  {
+},
+{
 "Year Published": 2010.0,
    "Title": "The Hunger Games",
    "Author": "Suzanne Collins",
    "Genre": "Science Fiction, Thriller, Dystopian Fiction",
    "Rating": "4.7/5",
    "Description": "The Hunger Games is a series of young adult dystopian novels written by American author Suzanne Collins. The first three novels are part of a trilogy following teenage protagonist Katniss Everdeen, and the fourth book is a prequel set 64 years before the original."
-     },
-  {
+},
+{
 "Year Published": 1960.0
    "Title": "To Kill a MockingBird",
    "Author": "Harper Lee",
    "Genre": "Fiction, Historical Fiction, Literature",
    "Rating": "4.26/5",
    "Summary": "Set in small-town Alabama, the novel is a bildungsroman, or coming-of-age story, and chronicles the childhood of Scout and Jem Finch as their father Atticus defends a Black man falsely accused of rape. Scout and Jem are mocked by classmates for this."
-     },
-   {
+},
+{
 "Year Published": 1954.0,
    "Title": "Lord of the Flies",
    "Author": "William Golding",
    "Genre": "Fiction, Classic, Literature",
    "Rating": "3.69/5",
    "Summary": "Lord of the Flies is about a group of British schoolboys who are stranded on an uninhabited island after their plane crashes. At first, they attempt to establish order and rules, but their society gradually descends into chaos and violence."
-     },
-   { 
+},
+{ 
 "Year Published": 1951.0,
    "Title": The Catcher in the Rye",
    "Author": "J.D. Salinger"
    "Genre": "Classics, Fiction, Literature",
    "Rating": "3.8/5",
    "Summary": "The novel details two days in the life of 16-year-old Holden Caulfield after he has been expelled from prep school. Confused and disillusioned, Holden searches for truth and rails against the “phoniness” of the adult world."
-     },
-   {
+},
+{
 "Year Published": 2023.0,
    "Title": "The 272",
    "Author": "Rachel L. Swarns",
    "Genre": "Biography, Genealogy",
    "Rating": "4.2/5", 
    "Summary": "The 272 tells a bigger story, demonstrating how slavery fueled the growth of the Catholic Church in America and bringing to light the enslaved people whose forced labor helped to build the largest religious denomination in the nation."
-      },
-    {
+},
+{
 "Year Published": 2023.0,
    "Title": "The Bee Sting",
    "Author": "Paul Murray",
    "Genre": "Humor, Psychological Fiction",
    "Rating": "4/5",
    "Summary": "In Murray’s boisterous tragicomic novel, a once wealthy Irish family struggles with both the aftermath of the 2008 financial crash and their own inner demons."
-      },
-    {
+},
+{
 "Year Published": 2023.0,
    "Title": "All The Sinners Bleed",
    "Author": "S.A. Cosby",
    "Genre": "Southern Thriller",
    "Rating": "",
    "Summary": "In his earlier thrillers, Cosby worked the outlaw side of the crime genre. In his new one — about a Black sheriff in a rural Southern town, searching for a serial killer who tortures Black children — he’s written a crackling good police procedural." 
-      },
-    {
+},
+{
 “Year Published”: 2023.0
    “Title”: "Biography of X", 
    “Author”: "Catherine Lacey",
    “Genre”: "Experimental Fiction",
    “Rating”: "3.9/5"
    “Summary”: "Lacey rewrites 20th-century U.S. history through the audacious fictional life story of X, a polarizing female performance artist who made her way from the South to New York City’s downtown art scene."
-      },
-    {
+},
+{
 “Year Published”: 2023.0 
    “Title”: "Chain-Gang All Stars", 
    “Author”: "Nana Kwame Adjei-Brenyah",
    “Genre”: "Literary Dystopia",
    “Rating”: ""
    “Summary”: "This satire — in which prison inmates duel on TV for a chance at freedom — makes readers complicit with the bloodthirsty fans sitting ringside. The fight scenes are so well written they demonstrate how easy it might be to accept a world this sick."
-      },
-    {
+},
+{
 “Year Published”: 2023.0 
    “Title”: "The Covenant of Water",
    “Author”: "Abraham Verghese",
    “Genre”: "Historical Fiction",
    “Rating”: ""
    “Summary”: "Verghese’s first novel since “Cutting for Stone” follows generations of a family across 77 years in southwestern India as they contend with political strife and other troubles — capped by a shocking discovery made by the matriarch’s granddaughter, a doctor."
-        },
+}
 ];
 
 let Books = BooksArray; 
