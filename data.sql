36ffd3d422cc0adccf832ca6720228eba81ba868
jcl386 <jcl386@drexel.edu>
2024-03-06 11:14:00 -0500

    new table

--- data.sql
+++ data.sql
@@ -8,6 +8,15 @@ CREATE TABLE books(
     summary TEXT NOT NULL
 );
 
+
+CREATE TABLE comments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  comment TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO books (title,genre,summary)
 VALUES
 ("book1","pg13","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
