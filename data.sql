49f1d8935651d39b90eb762d3f5b690cea7758dc
jcl386 <jcl386@drexel.edu>
2024-04-03 12:18:03 -0400

    modifying  data.sql

--- data.sql
+++ data.sql
@@ -1,14 +1,6 @@
 CREATE DATABASE website;
 USE website;
 
-CREATE TABLE books(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    genre VARCHAR(255) NOT NULL,
-    summary TEXT NOT NULL
-);
-
-
 CREATE TABLE comments (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user VARCHAR(255) NOT NULL,
@@ -17,7 +9,3 @@ CREATE TABLE comments (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO books (title,genre,summary)
-VALUES
-("book1","pg13","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-("book2","pg13","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
